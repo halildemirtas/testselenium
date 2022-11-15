2621c2a0f2028db46b3e8a75930d31283d820fb8
halildemirtas <102150259+halildemirtas@users.noreply.github.com>
2022-11-15 15:30:27 +0300

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //string dizgi ve katar
+        String x="udemy kursumuz";
+        x=x.toUpperCase();
+        System.out.println(x);
+
+    }
+}
